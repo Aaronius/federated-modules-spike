5010ecd912b8624e1a2f4488fd54a67b12f79156
Aaron Hardy <aaron.hardy@alteryx.com>
2023-04-25 21:46:33 -0600

    Revert changes

--- app1/src/getRoutes.js
+++ app1/src/getRoutes.js
@@ -5,7 +5,6 @@ import App2 from "./App2";
 import App3 from "./App3";
 
 const LazyAboutUs = React.lazy(() => import("./AboutUs"));
-
 const SuspenseContainer = ({ children }) => {
   return <React.Suspense fallback="Loading...">{children}</React.Suspense>;
 };
@@ -18,17 +17,12 @@ const getRoutes = async () => {
     }),
     importFederatedModule({
       remote: "app3",
-      module: "./getRoutes",
+      module: "getRoutes",
     }),
   ]);
 
   const app2Routes = getApp2RoutesModule.default();
   const app3Routes = getApp3RoutesModule.default();
-
-  const { default: getApp2Routes } = await importFederatedModule({
-    remote: "app2",
-    module: "./getRoutes",
-  });
 
   const routes = [
     {
--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
   },
   output: {
     publicPath: "auto",
-    uniqueName: "zzzzz",
   },
   module: {
     rules: [
